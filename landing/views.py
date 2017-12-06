199b64f79e6ac6292284e13ddbd49d9c08afd689
Luis Escobar <luis.latam@gmail.com>
2017-12-05 23:50:09 -0500

    Full html version

--- landing/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, url_for, render_template
-
-landing_app = Blueprint('landing_app', __name__)
-
-@landing_app.route('/')
-def init():
-    return render_template("landing/landing.html")
